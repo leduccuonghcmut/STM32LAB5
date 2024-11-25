9a8fec647c2766571f3b4136140440b127275ec4
Le Duc Cuong <leduccuonghcmut@gmail.com>
2024-11-26 00:33:54 +0700

    update

--- CubeIDE/Core/Src/main.c
+++ CubeIDE/Core/Src/main.c
@@ -60,6 +60,15 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+uint8_t temp = 0;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+	if(huart->Instance == USART2){
+		HAL_UART_Transmit(&huart2, &temp, 1, 50);
+		HAL_UART_Receive_IT(&huart2, &temp, 1);
+	}
+}
+
 /* USER CODE END 0 */
 
 /**

ca8d7ead477f4ba5cbfbb354085644f717706f02
Le Duc Cuong <leduccuonghcmut@gmail.com>
2024-11-27 13:26:57 +0700

    no message

--- CubeIDE/Core/Src/main.c
+++ CubeIDE/Core/Src/main.c
@@ -102,7 +102,7 @@ int main(void)
   MX_ADC1_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_UART_Receive_IT(&huart2, &temp, 1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
